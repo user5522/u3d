5b8d32642745f03d7b321d83207394975470f9f7
Username * <oueslatim268@gmail.com>
2023-02-07 21:22:16 +0100

    camera controller enhancements

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player; //Public variable to store a reference to the player game object
+    // Variables
+    public Transform player;
+    public float mouseSensitivity = 2f;
+    float cameraVerticalRotation = 0f;
 
-    private Vector3 offset; //Private variable to store the offset distance between the player and camera
-
-    // Use this for initialization
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        // Lock and Hide the Cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // LateUpdate is called after Update each frame
-    void LateUpdate()
+    void Update()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        // Collect Mouse Input
+
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Rotate the Camera around its local X axis
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        // Rotate the Player Object and the Camera around its Y axis
+
+        player.Rotate(Vector3.up * inputX);
     }
 }
