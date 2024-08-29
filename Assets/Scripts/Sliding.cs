0d4348d41d9e5a0558aa17430dcc793ea22e9e28
Username * <oueslatim@pm.me>
2024-08-29 17:53:04 +0100

    Update Sliding.cs

--- Assets/Scripts/Sliding.cs
+++ Assets/Scripts/Sliding.cs
@@ -5,32 +5,55 @@ public class Sliding : MonoBehaviour
     [Header("References")]
     public Transform orientation;
     public Transform playerObject;
+    public Transform cameraTransform;
+    public CameraController cam;
+    public PlayerController playerController;
     private Rigidbody rb;
-    private PlayerController playerController;
 
     [Header("Sliding")]
     public float slideForce;
-
     public float slideYscale;
+    public float rotationSpeed;
+
+    [Header("Ground Slam")]
+    public float groundSlamForce;
+    public float shakeDuration;
+    public float shakeIntensity;
+
     private float startYscale;
-    private float horizontalInput;
-    private float verticalInput;
+    private Vector3 slideDirection;
+    private bool isGroundSlamming = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        playerController = GetComponent<PlayerController>();
         startYscale = playerObject.localScale.y;
         playerController.sliding = false;
+        cameraTransform = Camera.main.transform;
+        cam.DoTilt(0f);
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (playerController.grounded && !playerController.sliding) StartSlide();
+            else if (!playerController.grounded) GroundSlam();
+        }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl) && (horizontalInput != 0 || verticalInput != 0)) StartSlide();
-        if (Input.GetKeyUp(KeyCode.LeftControl)) StopSlide();
+        if ((Input.GetKeyUp(KeyCode.LeftControl) && playerController.sliding)
+
+            || (!playerController.grounded && playerController.sliding))
+            StopSlide();
+        if (CanRestorePlayerHeight() && !playerController.sliding) RestorePlayerHeight();
+
+        if (playerController.sliding) HandleSlidingMovement();
+
+        if (isGroundSlamming && playerController.grounded)
+        {
+            cam.DoShake(shakeDuration, shakeIntensity);
+            isGroundSlamming = false;
+        }
     }
 
     void FixedUpdate()
@@ -43,22 +66,65 @@ public class Sliding : MonoBehaviour
         playerController.sliding = true;
         playerObject.localScale = new Vector3(playerObject.localScale.x, slideYscale, playerObject.localScale.z);
         rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        slideDirection = orientation.forward;
+        if (rb.velocity.y >= 0) slideDirection.y = 0;
+    }
+
+    private void GroundSlam()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(Vector3.down * groundSlamForce, ForceMode.Impulse);
+        isGroundSlamming = true;
+    }
+
+    private void HandleSlidingMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0)
+        {
+            if (horizontalInput < 0) cam.DoTilt(5f);
+            if (horizontalInput > 0) cam.DoTilt(-5f);
+            cam.DoTilt(0f);
+            Vector3 cameraRight = cameraTransform.right;
+            cameraRight.y = 0;
+            cameraRight.Normalize();
+            Vector3 turnDirection = horizontalInput == 0 ? orientation.forward : horizontalInput * cameraRight;
+            slideDirection = Vector3.RotateTowards(slideDirection, turnDirection, rotationSpeed * Time.deltaTime, 0f);
+            slideDirection.Normalize();
+        }
     }
 
     private void SlidingMovement()
     {
-        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        if (!playerController.OnSlope() || rb.velocity.y > -0.1f)
-            rb.AddForce(inputDirection.normalized * slideForce, ForceMode.Force);
-        else
-            rb.AddForce(playerController.GetSlopeMoveDirection(inputDirection) * slideForce, ForceMode.Force);
+        Vector3 forceToApply;
+        if (playerController.OnSlope())
+            forceToApply = playerController.GetSlopeMoveDirection(slideDirection) * slideForce;
+        else forceToApply = slideDirection * slideForce;
+        rb.AddForce(forceToApply, ForceMode.Force);
+        playerObject.forward = Vector3.Slerp(playerObject.forward, slideDirection, Time.deltaTime * 10f);
     }
-
 
     private void StopSlide()
     {
         playerController.sliding = false;
+        cam.DoTilt(0f);
+    }
+
+    public Vector3 GetSlideDirection() => slideDirection;
+
+    private bool CanRestorePlayerHeight()
+    {
+        Vector3 playerCenter = transform.position + (Vector3.up * (playerObject.localScale.y / 2f));
+        float playerHalfNormalHeight = startYscale / 2f;
+
+        RaycastHit hit;
+        if (Physics.Raycast(playerCenter, Vector3.up, out hit, playerHalfNormalHeight + 0.1f, playerController.groundLayer))
+            return hit.distance >= startYscale;
+        return true;
+    }
+
+    private void RestorePlayerHeight()
+    {
         playerObject.localScale = new Vector3(playerObject.localScale.x, startYscale, playerObject.localScale.z);
     }
 }
