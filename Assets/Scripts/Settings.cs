a2fa9c7ed620015accee2fac775b0933f1fe3a23
Username * <oueslatim@pm.me>
2024-09-01 18:28:03 +0100

    feat: quick settings implementation (from SquareGame)
    https://github.com/user5522/SquareGame/blob/main/Assets/Scripts/Settings.cs
    and
    https://github.com/user5522/SquareGame/blob/main/Assets/Scripts/SettingsManager.cs
    
    * modified slightly to fit my new needs

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Toggle vSyncToggle;
+    public Slider maxFPSSlider;
+    public TMP_Text maxFPSText;
+    public Dropdown resolutionDropdown;
+    public FullScreenMode fullScreenMode = FullScreenMode.FullScreenWindow;
+    public GameObject SettingsPanel;
+
+    private void Awake()
+    {
+        LoadSettings();
+
+        vSyncToggle.onValueChanged.AddListener(ToggleVSync);
+        maxFPSSlider.onValueChanged.AddListener(SetMaxFPS);
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+
+        SetupResolutionDropdown();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SettingsManager sm = SettingsManager.Instance;
+            sm.SetPauseState(!sm.GetPauseState());
+            SettingsPanel.SetActive(sm.GetPauseState());
+        }
+    }
+
+    private void SetupResolutionDropdown()
+    {
+        resolutionDropdown.ClearOptions();
+        Resolution[] resolutions = Screen.resolutions;
+        int maxResolutions = Mathf.Min(resolutions.Length, 20);
+        for (int i = 0; i < maxResolutions; i++)
+        {
+            Resolution resolution = resolutions[i];
+            Dropdown.OptionData option = new Dropdown.OptionData(
+                string.Format("{0}x{1}", resolution.width, resolution.height)
+            );
+            resolutionDropdown.options.Add(option);
+        }
+
+        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", 0);
+        resolutionDropdown.value = savedResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    private void LoadSettings()
+    {
+        bool vSyncOn = PlayerPrefs.GetInt("VSync", 1) == 1;
+        vSyncToggle.isOn = vSyncOn;
+        QualitySettings.vSyncCount = vSyncOn ? 1 : 0;
+
+        float maxFPS = PlayerPrefs.GetFloat("MaxFPS", 60f);
+        maxFPSSlider.value = maxFPS;
+        Application.targetFrameRate = (int)maxFPS;
+        maxFPSText.text = "FPS Limiter - " + maxFPS.ToString();
+
+        int resolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", 0);
+        SetResolution(resolutionIndex);
+    }
+
+    private void ToggleVSync(bool isOn)
+    {
+        QualitySettings.vSyncCount = isOn ? 1 : 0;
+        PlayerPrefs.SetInt("VSync", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    private void SetMaxFPS(float value)
+    {
+        Application.targetFrameRate = (int)value;
+        maxFPSText.text = "FPS Limiter - " + value.ToString();
+        PlayerPrefs.SetFloat("MaxFPS", value);
+        PlayerPrefs.Save();
+    }
+
+    private void SetResolution(int index)
+    {
+        Resolution[] resolutions = Screen.resolutions;
+        Screen.SetResolution(resolutions[index].width, resolutions[index].height, fullScreenMode);
+        PlayerPrefs.SetInt("ResolutionIndex", index);
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance { get; private set; }
+
+    private static bool IsPaused = false;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
+    }
+
+    private void Start() => LoadSettings();
+
+    private void Update() => UpdateCursorVisibility();
+
+    private void LoadSettings()
+    {
+        // VSync
+        bool vSyncOn = PlayerPrefs.GetInt("VSync", 1) == 1;
+        QualitySettings.vSyncCount = vSyncOn ? 1 : 0;
+
+        // Max FPS
+        float maxFPS = PlayerPrefs.GetFloat("MaxFPS", 60f);
+        Application.targetFrameRate = (int)maxFPS;
+
+        // Resolution
+        int resolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", 0);
+        Resolution[] resolutions = Screen.resolutions;
+        if (resolutionIndex < resolutions.Length)
+        {
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode);
+        }
+    }
+
+    private void UpdateCursorVisibility()
+    {
+        Cursor.visible = IsPaused;
+        Cursor.lockState = IsPaused ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+
+    public void SetPauseState(bool paused) => IsPaused = paused;
+    public bool GetPauseState() => IsPaused;
+}
